de83bde11274aa227d91ee52505dc1912ea53b0e
Aashish <aashish2000kumar@gmail.com>
2020-11-17 03:44:03 +0530

    final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,94 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue[800],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/164604.jpg'),
+              ),
+              Text('Aashish Jha',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.w300,
+                fontFamily: 'Pacifico',
+                ),
+              ),
+              Text('Flutter Developer',
+                style: TextStyle(
+                  color: Colors.teal[200],
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source Sans Pro',
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(Icons.call,
+                    size: 20.0,
+                    semanticLabel: 'Call',
+                    color: Colors.black87,
+                  ),
+                  title: Text('+917838345638',
+                    style: TextStyle(
+                      color: Colors.lightBlue[900],
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(Icons.attach_email_rounded,
+                    size: 20.0,
+                    semanticLabel: 'Call',
+                    color: Colors.black87,
+                  ),
+                  title: Text('aashish2000kumar@gmail.com',
+                    style: TextStyle(
+                      color: Colors.lightBlue[900],
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
